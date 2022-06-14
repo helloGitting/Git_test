69ad85c6afce40a95c715040a6f8af74eb66d1f8
admin <admin@qq.com>
2022-06-14 13:45:42 +0800

    IDEA third commit

--- src/main/java/com/atguigu/test1.java
+++ src/main/java/com/atguigu/test1.java
@@ -11,6 +11,7 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("GitTest1");
         System.out.println("GitTest2");
+        System.out.println("GitTest3");
     }
 }
 
