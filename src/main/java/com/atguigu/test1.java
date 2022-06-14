76106f4854d6173bd680d0300947967984400b84
admin <admin@qq.com>
2022-06-14 12:51:37 +0800

    IDEA first commit

---
+++ src/main/java/com/atguigu/test1.java
@@ -0,0 +1,40 @@
+package com.atguigu;
+
+/**
+ * @ClassName test1
+ * @Description
+ * @Author admin
+ * @Date 12:48 2022/6/14
+ * @Version 2.0
+ **/
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
