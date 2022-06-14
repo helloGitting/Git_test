365d5e9796f6a90649997e65909d21d17ab8ba1c
admin <admin@qq.com>
2022-06-14 13:43:45 +0800

    IDEA second commit

--- src/main/java/com/atguigu/test1.java
+++ src/main/java/com/atguigu/test1.java
@@ -9,7 +9,8 @@ package com.atguigu;
  **/
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("aaa");
+        System.out.println("GitTest1");
+        System.out.println("GitTest2");
     }
 }
 
